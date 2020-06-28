184b605b65ca84d1f3397cbdd1bdc900bcace495
vagrant <vagrant@localhost.localdomain>
2020-06-28 08:00:09 +0000

    [update]meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0d9207f1059da45043a2bb91f175d6908c29ca6c7d339530cd1997f72ce09462c0e2c9a6cc6679d57ba45196db61bdbfc841d405b69d1d0556147c231b706525'
