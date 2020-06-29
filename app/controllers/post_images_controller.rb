699386ee2d6bc09959cf98ea3a673c499e5ac5e1
vagrant <vagrant@localhost.localdomain>
2020-06-29 06:07:12 +0000

    [update]meshiterro

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -17,7 +17,7 @@ class PostImagesController < ApplicationController
 
     def show
         @post_image = PostImage.find(params[:id])
-        @post_comment = PostCommnet.new
+        @post_comment = PostComment.new
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show] do
     resources :post_comments, only: [:create, :destroy]
-  end
+end
 end
